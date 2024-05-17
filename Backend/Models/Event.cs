3083670b708d37e9a2533a2150287be507859771
hagrid862 <n.okroj@icloud.com>
2024-05-17 08:27:18 +0200

    Added Event Model and some updates

---
+++ Backend/Models/Event.cs
@@ -0,0 +1,14 @@
+namespace Backend.Models;
+
+public class Event
+{
+    public required long Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public int Time { get; set; }
+    public List<DateTime> Dates { get; set; }
+    public required float Price { get; set; }
+    public required Category Category { get; set; }
+    public List<Tag>? Tags { get; set; }
+    public List<Review> Reviews { get; set; }
+}
