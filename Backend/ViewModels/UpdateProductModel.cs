5bc480fe87f5da0c2ec8ec244defbe05852bd0aa
hagrid862 <n.okroj@icloud.com>
2024-06-17 21:34:38 +0200

    fix: removed RemovedImages from UpdateProduct view model

--- Backend/ViewModels/UpdateProductModel.cs
+++ Backend/ViewModels/UpdateProductModel.cs
@@ -14,5 +14,4 @@ public class UpdateProductModel
     public float? Price { get; set; }
     public string? CategoryId { get; set; }
     public List<string>? Tags { get; set; }
-    public string? RemovedImages { get; set; }
 }
