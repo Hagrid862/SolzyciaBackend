4eb2b2e9cc0e24f8834544862a4ecd8ad649c473
hagrid862 <n.okroj@icloud.com>
2024-07-15 17:39:34 +0200

    chore: added migrations to remove useless model and add additional info field to event location

---
+++ Backend/Migrations/20240715132242_removedUselessLocationModelAndAddedAdditionalInfoToEventLocationModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedUselessLocationModelAndAddedAdditionalInfoToEventLocationModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalInfo",
+                table: "EventLocation",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalInfo",
+                table: "EventLocation");
+        }
+    }
+}
